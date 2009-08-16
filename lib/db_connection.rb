b9956915dd4d64ce83c8b329c72155150f279e4a
Tony Collen <tony@halogenlabs.com>
2009-08-16 18:39:02 -0500

    clean up db_Connection slightly

--- lib/db_connection.rb
+++ lib/db_connection.rb
@@ -2,4 +2,4 @@ require 'dm-core'
 require 'dm-aggregates'
 
 DataMapper::Logger.new(App::Config[:log_file], App::Config[:log_level])
-DataMapper.setup(:default, YAML::load(IO.read(App::Config[:db_config])))
+DataMapper.setup(:default, YAML::load_file(App::Config[:db_config]))
