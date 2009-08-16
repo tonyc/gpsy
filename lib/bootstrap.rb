c91824dad1e63b26f397b01c441c3406c8156f6e
Tony Collen <tony@halogenlabs.com>
2009-08-16 18:34:13 -0500

    move db.rb to db_connection.rb

--- lib/bootstrap.rb
+++ lib/bootstrap.rb
@@ -1,14 +1,11 @@
 require 'rubygems'
 require 'extlib'
 
-require 'dm-core'
-require 'dm-aggregates'
-
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'app'
 require 'app_config'
-require 'db'
+require 'db_connection'
 
 # Autoload any models
 Dir.glob("app/models/*.rb") do |model_path|
