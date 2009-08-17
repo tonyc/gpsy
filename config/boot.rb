43e5eb87165cda8731a69d55698fcde70662bd08
Tony Collen <tony@halogenlabs.com>
2009-08-16 20:53:29 -0500

    remove unneeded extlib require since dm-core already does

--- config/boot.rb
+++ config/boot.rb
@@ -1,12 +1,13 @@
 $:.unshift File.join(File.dirname(__FILE__), "..", "lib")
 
 require 'rubygems'
-require 'extlib'
 
+# app dependencies
 require 'dm-core'
 require 'dm-aggregates'
 require 'dm-validations'
 
 require 'app'
 
+# This is the slow way of loading models
 Dir["#{App.root}/app/models/*.rb"].sort.each { |f| require f }
